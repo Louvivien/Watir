d9512f00c7396266a7daa3890d4839f3d597f6e0
kalison <lamond.tada@gmail.com>
2017-07-24 15:48:38 +0200

    OK

---
+++ watir.rb
@@ -0,0 +1,19 @@
+require 'watir'
+browser = Watir::Browser.new()
+browser.goto 'google.com'
+search_bar = browser.text_field(class: 'gsfi')
+search_bar.set("Hello World!")
+
+
+search_bar.send_keys(:enter)
+
+
+browser.driver.manage.timeouts.implicit_wait = 3
+
+search_result_divs = browser.divs(class:"rc")
+search_result_divs.each { |div| p div.h3.text }
+
+
+
+p ""
+browser.close
